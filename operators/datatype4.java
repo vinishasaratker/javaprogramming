8abfce29421e161064f0fc8fc37a29fc4bb7d383
vinishasaratker <vinishasaratker@gmail.com>
2025-09-25 10:37:16 +0530

    my

---
+++ operators/datatype4.java
@@ -0,0 +1,17 @@
+class datatype4 {
+
+public static void main(String[] args){
+System.out.println("hiii");
+// jo byte short int long hota hai vo integer store krte hai short short value ko or long or int higher values ko store krte hai
+byte b=1;
+short s=78;
+int n=90;
+long l=889;
+
+char c='c';
+float f=90.09f;// jab bhi float me koi value likhte hai to uske bad f lgana must hota hai
+double d=865.9655;
+boolean status=true;// or boolean ko pura likhna hota hai
+System.out.println(b+" "+s+" "+n+" "+l+" "+c+" "+f+" " +d+" "+status+" " );
+}
+}
