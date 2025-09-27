ffaa40fa0f18ba2889b4e4478b06d33e2028dec6
vinishasaratker <vinishasaratker@gmail.com>
2025-09-27 16:15:53 +0530

    mango

--- ifelse.java
+++ ifelse.java
@@ -33,26 +33,74 @@
 //  }
     
 // }
-import java.util.Scanner;
-class ifelse
-{
-    public static void main(String[] args) {
-     Scanner obj=new Scanner (System.in);
-        int a;
-    System.out.println("enter number 1st");
-    a =obj.nextInt();
+// import java.util.Scanner;
+// class ifelse
+// {
+//     public static void main(String[] args) {
+//      Scanner obj=new Scanner (System.in);
+//         int a;
+//     System.out.println("enter number 1st");
+//     a =obj.nextInt();
 
-    int b;
-    System.out.println("enter second number");
-    b=obj.nextInt();
-    int c;
-    System.out.println("enter third number");
-    c=obj.nextInt();
-    if((a*a)+(b*b)==(c*c)){
-System.out.println("pythagorous");
-    }
-    else{
-        System.out.println(" not pythagorous" );
-    }
+//     int b;
+//     System.out.println("enter second number");
+//     b=obj.nextInt();
+//     int c;
+//     System.out.println("enter third number");
+//     c=obj.nextInt();
+//     if((a*a)+(b*b)==(c*c)){
+// System.out.println("pythagorous");
+//     }
+//     else{
+//         System.out.println(" not pythagorous" );
+//     }
+//     }
+// }
+// import java.util. Scanner;
+// class ifelse
+// {
+//     public static void main(String[] args) {
+        
+//       Scanner obj=new Scanner(System.in);  
+//       int day;
+//       System.out.println(" enter any day");
+//       day=obj.nextInt();
+//       if(day==1){
+//         System.out.println("monday");
+//       }
+//       else if(day==2){
+//         System.out.println("tuesday");
+//       }
+
+//       else if(day==3){
+//         System.out.println("wednesday");
+//       }
+//       else if(day==4){
+//         System.out.println("thursday");
+//       }
+//             else if(day==5){
+//         System.out.println("friday");
+//       }
+//       else if(day==6){
+//         System.out.println("saturday");
+//       }
+//             else if(day==7){
+//         System.out.println("sunday");
+//       }
+      
+
+
+//     }
+// }
+class ifelse{
+    public static void main(String[] args) {
+      int z=5;
+      int p=9;
+      int temp;
+      temp=z;
+      z=p;
+     p=temp;
+      System.out.println(z);  
+      System.out.println(p);
     }
 }
