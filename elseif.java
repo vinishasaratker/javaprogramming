f0ee83dac4af1d179bad4200456a37f475382c3e
vinishasaratker <vinishasaratker@gmail.com>
2025-10-05 14:40:23 +0530

    jindagi hospital

--- elseif.java
+++ elseif.java
@@ -107,103 +107,128 @@
 //     }
 // }
 
-// import java.util.Scanner;
-// public class elseif{
-//     public static void main(String[] args) {
+// // import java.util.Scanner;
+// // public class elseif{
+// //     public static void main(String[] args) {
         
-//         Scanner obj=new Scanner(System.in);
-//         int unit;
-//         System.out.println(" enter units ");
-//         unit=obj.nextInt();
-//         if(unit<=100){
-//             int bill;
-//             bill=unit*5;
-//             System.out.println("your bill is +"+bill);
-//         }
-//         else if(unit>100 && unit<=200){
-//             int bill;
-//             bill=unit*7;
-//             System.out.println("your bill is +"+bill);
-//         }
-//          else{
-//             int bill;
-//             bill=unit*10;
-//             System.out.println("your bill is +"+bill);
-//         }
+// //         Scanner obj=new Scanner(System.in);
+// //         int unit;
+// //         System.out.println(" enter units ");
+// //         unit=obj.nextInt();
+// //         if(unit<=100){
+// //             int bill;
+// //             bill=unit*5;
+// //             System.out.println("your bill is +"+bill);
+// //         }
+// //         else if(unit>100 && unit<=200){
+// //             int bill;
+// //             bill=unit*7;
+// //             System.out.println("your bill is +"+bill);
+// //         }
+// //          else{
+// //             int bill;
+// //             bill=unit*10;
+// //             System.out.println("your bill is +"+bill);
+// //         }
 
-//     }
+// //     }
     
 
-// }
-// import java.util.Scanner;
-// public class elseif{
-//     public static void main(String[] args) {
+// // }
+// // import java.util.Scanner;
+// // public class elseif{
+// //     public static void main(String[] args) {
         
-// //         Library Fine Calculation If book is returned: Within 7 days → no fine.
-// // 8–15 days late → ₹2 per day fine. More than 15 days late → ₹5 per day fine.
+// // //         Library Fine Calculation If book is returned: Within 7 days → no fine.
+// // // 8–15 days late → ₹2 per day fine. More than 15 days late → ₹5 per day fine.
 
-// int days;
-// Scanner obj=new Scanner(System.in);
-// System.out.println("enter days of book");
-// days=obj .nextInt();
-// if(days<=7){
-//     System.out.println(" there is no  extra charges");
-// }
-// else if(days>=8 && days<=15){
-//     int fine;
-//     fine=(days-7)*2;
-//     System.out.println(" extra charges is" +" "+ fine);
-//     }
+// // int days;
+// // Scanner obj=new Scanner(System.in);
+// // System.out.println("enter days of book");
+// // days=obj .nextInt();
+// // if(days<=7){
+// //     System.out.println(" there is no  extra charges");
+// // }
+// // else if(days>=8 && days<=15){
+// //     int fine;
+// //     fine=(days-7)*2;
+// //     System.out.println(" extra charges is" +" "+ fine);
+// //     }
 
+// //     else{
+// //         int fine;
+// //         fine=(days-7)*5;
+// //         System.out.println(" extra charges is+"+" "+fine);
+// //     }
+// // }}
+
+// import java.util.Scanner;
+//  public  class elseif{
+//     public static void main(String[] args) {
+//         Scanner obj=new Scanner(System.in);
+//     int option;
+//     System.out.println("enter option 1. for f-c & 2. for c-f");
+//     option= obj .nextInt();
+//  if(option==1){
+//         int temp;
+//         System.out.println("enter tempreture in  fehrenite");
+//         temp=obj.nextInt();
+//         int ans;
+//         ans=(temp-32)*5/9;
+//         System.out.println(ans+" "+"tempreture in celsius");
+// if(ans<10){
+//             System.out.println("collder");
+//         }
+//         else if(ans>=10 && ans<20){
+//               System.out.println("cold");
+//         }
+//         else if(ans>=20 && ans<30){
+//               System.out.println("hot");}}
+//     else if(option==2){
+//          int temp;
+//         System.out.println("enter tempreture in  celsius");
+//         temp=obj.nextInt();
+//         int ans;
+//         ans=((temp*5)/9)+32;
+//         System.out.println(ans+" "+"tempreture in fahrenite");
+
+//         if(ans<10){
+//             System.out.println("collder");}
+//         else if(ans>=10 && ans<20){
+//               System.out.println("cold");}
+//         else if(ans>=20 && ans<30){
+//               System.out.println("hot");}}
 //     else{
-//         int fine;
-//         fine=(days-7)*5;
-//         System.out.println(" extra charges is+"+" "+fine);
+//         System.out.println("invalid"); }
+//  }}
+ // write a program to convert meter into cm
+
+ // convert  byte into kilobyte
+
+//   import java.util.Scanner;
+// public class elseif{
+//      public static void main(String[] args)
+//      {
+        
+//         int  meter;
+//         Scanner obj=new Scanner(System.in);
+//         System.out.println(" enter length into meter");
+//         meter=obj .nextInt();
+//         int cm;
+//         cm=meter*100;
+//         System.out.println(cm);
+//      }
+    
 //     }
-// }}
-
-
-
-import java.util.Scanner;
- public  class elseif{
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-    int option;
-    System.out.println("enter option 1. for f-c & 2. for c-f");
-    option= obj .nextInt();
- if(option==1){
-        int temp;
-        System.out.println("enter tempreture in  fehrenite");
-        temp=obj.nextInt();
-        int ans;
-        ans=(temp-32)*5/9;
-        System.out.println(ans+" "+"tempreture in celsius");
-if(ans<10){
-            System.out.println("collder");
-        }
-        else if(ans>=10 && ans<20){
-              System.out.println("cold");
-        }
-        else if(ans>=20 && ans<30){
-              System.out.println("hot");}}
-    else if(option==2){
-         int temp;
-        System.out.println("enter tempreture in  celsius");
-        temp=obj.nextInt();
-        int ans;
-        ans=((temp*5)/9)+32;
-        System.out.println(ans+" "+"tempreture in fahrenite");
-
-        if(ans<10){
-            System.out.println("collder");
-        }
-        else if(ans>=10 && ans<20){
-              System.out.println("cold");
-        }
-        else if(ans>=20 && ans<30){
-              System.out.println("hot");
-        }}
-    else{
-        System.out.println("invalid");
-    }
- }}
+//       import java.util.Scanner;
+// public class elseif{
+//      public static void main(String[] args)
+//      { int by;
+//         System.out.println("enter lenghtt");
+//         Scanner obj=new Scanner(System.in);
+//         by=obj .nextInt();
+//         float kb;
+// kb=by/1024;
+// System.out.println(kb);
+// }
+// }
---
+++ hospitalque.java
@@ -0,0 +1,17 @@
+import java.util .Scanner;
+public class hospitalque {
+  public static void main(String[] args) {
+    
+     Scanner obj=new Scanner (System.in);
+     String patient;
+     System.out.println("enter patient condition  ");
+     patient=obj.next();
+     String ward;
+     System.out.println("enter ward number according to patient condition");
+     ward=obj.next();
+     int bil;
+     System.out.println("enter bill according towards");
+     bil=obj.nextInt();
+     if(patient=="CVR")
+  }  
+}
