7b0ca69ee15d4df1e793476ef25763df035456fe
vinishasaratker <vinishasaratker@gmail.com>
2025-10-14 22:21:29 +0530

    hy

--- loops/loop1.java
+++ loops/loop1.java
@@ -13,21 +13,55 @@
   import java.util.Scanner;
   class loop1{
     public static void main(String[] args) {
-        int i=9;
-        do{
-            System.out.println(i++);
-        }
-        
-        while(i<=19);
-        System.out.println("               ");
-        int b=2;
-        while(b<9){
-            System.out.println(b++);
-        }
-      System.out.println();
-        for(int v=1;v<=15;v++){
-            System.out.println(v);
-        }
+    //     int i=9;
+    //     do{
+    //         System.out.println(i++);}
+    //      while(i<=19);
+    //     System.out.println("   ");
+    //     int b=2;
+    //     while(b<9){
+    //         System.out.println(b++);
+    //     }
+    //   System.out.println();
+    //     for(int v=1;v<=15;v++){
+    //         System.out.println(v);
+    //     }
+    // Scanner obj=new Scanner(System.in);
+    // System.out.println(" enter any number");
+    // int n=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n*i);
+    // }
+    //     System.out.println(" enter any number 2");
+    // int n2=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n2*i);
+    // }
 
+    // int n3=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n3*i);
+    // }
+    // int n4=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n4*i);
+    // }
+    // int n5=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n5*i);
+    // }
+    // int n6=obj.nextInt();
+    // for(int i=1;i<=10;i++){
+    //     System.out.println(n6*i);
+    // }
+
+    int m=5;
+    int fact=1;
+    for(int i=1;i<=5;i++){
+         fact=fact*i;
+         
+      
+    }
+    System.out.println(fact);
     }
   }
