7ee8ea5685382b2d039d0f84f402b65bb2458880
vinishasaratker <vinishasaratker@gmail.com>
2025-11-06 22:28:46 +0530

    databse

---
+++ arrayy/basicsss.java
@@ -0,0 +1,27 @@
+// import java.util.Scanner;
+
+public class basicsss {
+   public static void main(String[] args) {
+        //  int arr1[]={1,2,3,5,7};
+    //  // array me subscript khi bhi lga skte hai like [] aaar,arrr[]
+    //  System.out.println(arr1[4]);
+    //  System.out.println(arr1.length);
+    //  int n=arr1.length;
+    //  for(int i=1;i<=n;i++){
+    //     System.out.println(arr1[i]);
+
+   //  }
+// use input in arrray
+   // Scanner obj=new Scanner(System.in);
+   // System.out.println(" enter array size");
+   // int n=obj.nextInt();
+   // reverse of an array 
+   //palindrom of anarray check it is palindrom or not
+       
+      int arr1[]={1,2,3,5,7};
+   int sum=0;
+     for(int i=0;i<5;i++){
+    sum=sum+arr1[i]; }
+    System.out.println(sum);
+} 
+}
