dc4f5a8bf16af5ec47d142e143393cd19837f384
vinishasaratker <vinishasaratker@gmail.com>
2025-11-07 10:21:15 +0530

    functions

---
+++ arrayy/basicfunction.java
@@ -0,0 +1,8 @@
+public class basicfunction {
+    public static void main(String[] args) {
+        int array[]={23,56,798,98};
+        System.out.println(array[3]);
+        System.out.println(array[1]);
+        System.out.println(array[0]);
+    }
+}
