cbc0b1c2ea2d4ffc2d20b38934b24b4ae610f437
vinishasaratker <vinishasaratker@gmail.com>
2025-11-09 16:25:51 +0530

    questions

--- arrayy/homework2.java
+++ arrayy/homework2.java
@@ -3,10 +3,12 @@ public class homework2 {
         int temp=arr[0];
 for(int i=1;i<arr.length-1;i++){
     boolean dup=false;
+
 for(int j=i+1;j<arr.length;j++){
     if(arr[i]==arr[j]&& arr[i]!=-1){
        arr[j]=-1;
        dup=true;
+       
     }
     if(dup){
         System.out.println(arr[i]);
