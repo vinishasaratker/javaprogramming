c4aa6a9336d281004dba38ee8753efb8e696a53b
vinishasaratker <vinishasaratker@gmail.com>
2025-11-10 10:21:08 +0530

    swap alternate

--- arrayy/rev.java
+++ arrayy/rev.java
@@ -16,8 +16,23 @@ public class rev {
         }
 
     }
+    static void swapalternate(int arrayy[]){
+int n=arrayy.length;
+for(int i=0;i<n;i+=2){
+    if(i+1<n){
+      int temp=arrayy[i];
+        arrayy[i]=arrayy[i+1];
+        arrayy[i+1]=temp;}
+       }
+   for(int i = 0; i < n; i++){
+        System.out.println(arrayy[i]);
+    }
+
+    }
     public static void main(String[] args) {
         int arr4[]={1,2,3,4,5};
         reverse(arr4);
+        int arrayy[]={  5,82,44,56,6,7};
+        swapalternate(arrayy);
     }
 }
