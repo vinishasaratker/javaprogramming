dcee0932f2658f95a6786f8ec24f6e309f96f4b7
vinishasaratker <vinishasaratker@gmail.com>
2025-11-17 12:51:50 +0530

    strings

--- strings/basic.java
+++ strings/basic.java
@@ -1,20 +1,38 @@
 public class basic {
     public static void main(String[] args) {
-        String name = "vinisha";
-        char arr[] = name.toCharArray();
-        int n = arr.length;
-        int start = 0;
-        int end = n - 1;
+    //     String name = "vinisha";
+    //     char arr[] = name.toCharArray();
+    //     int n = arr.length;
+    //     int start = 0;
+    //     int end = n - 1;
+    //     while (start < end) {
+    //         char temp = arr[start];
+    //         arr[start] = arr[end];
+    //         arr[end] = temp;
+    //         start++;
+    //         end--;}
+    // String rev = new String(arr);
+    //     System.out.println("reverse string is "+rev);
 
-        while (start < end) {
-            char temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    String name="madam";
+    char ar[]=name.toCharArray();
+    int start=0;
+    int end=ar.length-1;
+    boolean pallindrom=true;
+    while(start<end){
+        if(ar[start]!=ar[end]){
+            pallindrom=false;
+break;
         }
+        start++;
+        end--;
+    } 
+    if(pallindrom){
+        System.out.println("palindrom");
+    }
+    else{
+        System.out.println("not pallindrom");
+    }
 
-        String rev = new String(arr);
-        System.out.println("reverse string is "+rev);
     }
 }
---
+++ strings/stringgmethod.java
@@ -0,0 +1,11 @@
+public class stringgmethod {
+    public static void main(String[] args) {
+       String name1="vini";
+       for(int i=0;i<name1.length();i++){
+        System.out.println(name1.charAt(i));
+       }
+
+
+    }
+    
+}
