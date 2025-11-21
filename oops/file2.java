2f5d993756de42f6cff1f4944d1b8d9725d7f700
vinishasaratker <vinishasaratker@gmail.com>
2025-11-21 09:29:45 +0530

    object

---
+++ oops/file2.java
@@ -0,0 +1,12 @@
+
+
+public class file2 {
+    
+    public static void main(String[] args) {
+        Hero obj = new Hero();   // Hero class ka object
+        obj.display();           // Hero wali method call
+    }
+}
+
+    
+
---
+++ oops/method1.java
@@ -0,0 +1,14 @@
+class bike{
+    int price=90000;
+     String quality ="nice";
+
+}
+
+public class method1 {
+    
+    public static void main(String[] args) {
+        
+        bike obj=new bike();
+        System.out.println();
+    }
+}
