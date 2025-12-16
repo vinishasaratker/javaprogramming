2c0f9ac46b15cf972071f8414eef80a9d6665fb7
vinishasaratker <vinishasaratker@gmail.com>
2025-12-16 14:11:02 +0530

    hyy

--- strings/basicstring.java
+++ strings/basicstring.java
@@ -5,6 +5,8 @@ public class basicstring {
         char arr2[]={'a','b','c','d'};
         String str1="vinisha";
         String str2=new String(" vinishaa ");
-        
+        String fullname=str1+" "+str2;
+        System.out.println(fullname);
+        System.out.println(fullname.charAt(7));
     }
 }
