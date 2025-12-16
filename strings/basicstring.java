c2d014cfb6305a5a1c26d514bd5849b1865db139
vinishasaratker <vinishasaratker@gmail.com>
2025-12-16 12:23:04 +0530

    strings

---
+++ strings/basicstring.java
@@ -0,0 +1,10 @@
+public class basicstring {
+    
+    public static void main(String[] args) {
+        
+        char arr2[]={'a','b','c','d'};
+        String str1="vinisha";
+        String str2=new String(" vinishaa ");
+        
+    }
+}
