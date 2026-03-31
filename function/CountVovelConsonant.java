b29f885b8803da9fe4e012335e1e520b83b2cc35
vinishasaratker <vinishasaratker@gmail.com>
2026-03-31 17:03:18 +0530

    code

--- function/CountVovelConsonant.java
+++ function/CountVovelConsonant.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class CountVovelConsonant {
        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
@@ -12,8 +11,7 @@ public class CountVovelConsonant {
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') { // check alphabet
+ if (ch >= 'a' && ch <= 'z') { // check alphabet
                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                     vowels++;
                 } else {
--- function/printingpatterns.java
+++ function/printingpatterns.java
@@ -1,4 +1,4 @@
-public class printingpatterns{
+public class printingpatterns {
 
 static void pp( int n){
     for(int  r=1;r<=n;r++){
@@ -9,33 +9,28 @@ static void pp( int n){
         
         else{
             System.out.print(" ");
-        
-    }
-    System.out.println();}
-    }
-    static void right( int n){
-    for(int  r=1;r<=n;r++){
-        for(int c=1;c<=n;c++){
-            if(c>=r){
-                System.out.print("");
-            }
-        
-        else{
-            System.out.print("*");
         }
-    }
     System.out.println();
     }
+    }
 
-
-}
-
+    static void right(int n) {
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= n; c++) {
+                if (c >= r) {
+                    System.out.print("");
+                }
+                else {
+                    System.out.print("*");
+                 }
+            }
+            System.out.println();
+         }
+      }    
 
     public static void main(String[] args) {
-       pp(5);   
-       right(6);
-      
-    
-   
-}
+        pp(5);
+        right(6);
+
+    }
 }
