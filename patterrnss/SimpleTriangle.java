e067d50f20abd8234ea1d1e643141bb7c1959c07
vinishasaratker <vinishasaratker@gmail.com>
2026-05-14 23:29:19 +0530

    pattern

--- patterrnss/SimpleTriangle.java
+++ patterrnss/SimpleTriangle.java
@@ -1,12 +1,28 @@
 public class SimpleTriangle{
-    public static void main(String[] args) {
-     int n = 5;
 
-    for(int i = 1; i <= n; i++) {
+     public static void righttriangle(int n){
+ for(int i = 1; i <= n; i++) {
     for(int j = 1; j <= i; j++) {
     System.out.print("* ");
      }
      System.out.println(); 
      }
+        }
+
+        public static void invertedright(int l){
+for(int i=0;i<l;i++){
+    for(int j=i;j<l;j++){
+        System.out.println("  ");
+    }
+    System.out.println();
+    
+}
+        } 
+    public static void main(String[] args) {
+
+       int n=5;
+     righttriangle(n);
+     int l=7;
+     invertedright(l);
  }
 }
