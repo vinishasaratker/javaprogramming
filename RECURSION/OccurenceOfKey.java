264a1667ea4af17b5d98cd3c0a185bccf26a2c1f
vinishasaratker <vinishasaratker@gmail.com>
2026-05-18 22:18:36 +0530

    tilling problem

--- RECURSION/OccurenceOfKey.java
+++ RECURSION/OccurenceOfKey.java
@@ -9,8 +9,24 @@ public class OccurenceOfKey {
         }
         return firstoccurence(arr, key, i+1);
     }
+
+
+public static int tilingproblrm(int n){
+if(n==1 || n==0){
+    return 1 ;
+}
+
+    int fm1=tilingproblrm(n-1);
+    int fm2=tilingproblrm(n-2);
+    int total=fm1+fm2;
+    return total ;
+}
+
     public static void main(String[] args) {
          int arr[]={2,4,5,6,7,5,99,9,0};
          System.out.println(firstoccurence(arr, 5, 0));
+
+
+System.out.println(tilingproblrm(4));
     }
 }
