d26df3e84542bba0af5d132bcaf2d4a8f1b847b6
vinishasaratker <vinishasaratker@gmail.com>
2026-06-11 22:36:27 +0530

    linkedlist

---
+++ ArrayList/LinkedList/AddInLL.java
@@ -0,0 +1,36 @@
+package ArrayList.LinkedList;
+import java.util.LinkedList;
+public class AddInLL {
+
+public static class Node{
+int data;
+Node next ;
+public Node(int data){
+    this.data=data;
+    this.next=null;
+}
+    }
+
+    public static Node head ;
+    public static Node tail ;
+public void addFirst(int data){
+    //step 1 --- create new node 
+    Node newNode=new Node(data);
+    if(head==null){
+        head=tail=newNode;
+        return ;
+    }
+
+    // step 2-- newNode next= head 
+    newNode.next=head ;// link 
+
+    // step 3 -- head = newNode 
+    head=newNode ;
+}
+
+    public static void main(String[] args) {
+        LinkedList ll=new LinkedList<>();
+        ll.addFirst(1);
+        ll.add(9);
+    }
+}
---
+++ ArrayList/MonotoniQue.java
@@ -0,0 +1,88 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java .util.Collections;
+
+public class MonotoniQue {
+
+    public static boolean monotonicArray(ArrayList<Integer> list){
+
+        boolean increasing = true;
+        boolean dec = true;
+
+        for(int i=0; i<list.size()-1; i++){
+
+            if(list.get(i) > list.get(i+1))
+                increasing = false;
+
+            if(list.get(i) < list.get(i+1))
+                dec = false;
+        }
+// agr increasing ya decreasing dono me se ek  bhi true hai to answer will be true 
+        return increasing || dec;
+    }
+
+   
+
+    public static ArrayList<Integer> findLonely(ArrayList<Integer> nums) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        Collections.sort(nums);
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            boolean isLonely = true;
+
+            // Check duplicate on left
+            if (i > 0 && nums.get(i).equals(nums.get(i - 1))) {
+                isLonely = false;
+            }
+
+            // Check duplicate on right
+            if (i < n - 1 && nums.get(i).equals(nums.get(i + 1))) {
+                isLonely = false;
+            }
+
+            // Check x-1
+            if (i > 0 && nums.get(i) - nums.get(i - 1) == 1) {
+                isLonely = false;
+            }
+
+            // Check x+1
+            if (i < n - 1 && nums.get(i + 1) - nums.get(i) == 1) {
+                isLonely = false;
+            }
+
+            if (isLonely) {
+                ans.add(nums.get(i));
+            }
+        }
+
+        return ans;
+    }
+    
+
+   
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(0);
+
+        System.out.println(monotonicArray(list));
+
+        ArrayList<Integer> nums=new ArrayList<>();
+       nums.add(10);
+        nums.add(6);
+        nums.add(5);
+        nums.add(8);
+System.out.println(findLonely(nums));
+    }
+}
