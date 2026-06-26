a326f034a74b262d117446ab6daf4347d419a120
vinishasaratker <vinishasaratker@gmail.com>
2026-06-26 10:12:58 +0530

    formating

--- arrayy/PrintingAllPair.java
+++ arrayy/PrintingAllPair.java
@@ -1,12 +1,13 @@
 public class PrintingAllPair {
     public static void main(String[] args) {
-       int arry[]={2,3,4,5,6,1,9};
-       for(int i=0;i<arry.length;i++){
-        int num=arry[i];
-        for(int j=i+1;j<arry.length;j++){
-            System.out.println("("+num+ " ,"+ arry[j]+ ")" );
+        int arry[] = { 2, 3, 4, 5, 6, 1, 9 };
+        for (int i = 0; i < arry.length; i++) {
+            int num = arry[i];
+            for (int j = i + 1; j < arry.length; j++) {
+                System.out.println("(" + num + " ," + arry[j] + ")");
+            }
+            System.out.println();
+
         }
-        System.out.println();
-        
-       }  }
+    }
 }
