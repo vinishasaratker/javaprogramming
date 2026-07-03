23a0b41b6c7ec064fe96b77a594e825992d99241
vinishasaratker <vinishasaratker@gmail.com>
2026-07-03 18:01:55 +0530

    stacks

---
+++ Stacks/StackbyArraylist.java
@@ -0,0 +1,57 @@
+package Stacks;
+
+import java.util.ArrayList;
+
+public class StackbyArraylist {
+
+    static class stackb {
+
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        // Check if stack is empty
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        // Push operation
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        // Pop operation
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // Peek operation
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        stackb sb = new stackb();
+
+        sb.push(5);
+        sb.push(6);
+        sb.push(8);
+
+        System.out.println("Elements popped from stack:");
+
+        while (!sb.isEmpty()) {
+            System.out.println("Peek : " + sb.peek());
+            System.out.println("Pop  : " + sb.pop());
+        }
+    }
+}
