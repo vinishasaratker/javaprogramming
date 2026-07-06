ddd009bdc766f9df3d96ad05d18c1a1dcc7597c4
vinishasaratker <vinishasaratker@gmail.com>
2026-07-06 19:57:32 +0530

    pushat bottom bt stack

---
+++ Stacks/PushAtBottom.java
@@ -0,0 +1,25 @@
+package Stacks;
+import java.util.*;
+public class PushAtBottom {
+
+    public static void pushBottom(Stack<Integer> s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return ;
+        }
+        int top=s.pop();
+        pushBottom(s,data);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+     Stack<Integer> s=new Stack<>();
+     s.push(2);
+     s.push(8);
+     s.push(78);
+     pushBottom(s,4);
+     while(!s.isEmpty()){
+        System.out.println(s.pop() );
+     }
+        
+    }
+}
