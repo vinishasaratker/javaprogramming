8c004956e55a5fb1756d16a2dc95cd65f9c862ca
vinishasaratker <vinishasaratker@gmail.com>
2026-07-06 20:29:12 +0530

    stacks

--- Stacks/PushAtBottom.java
+++ Stacks/PushAtBottom.java
@@ -1,25 +1,28 @@
 package Stacks;
+
 import java.util.*;
+
 public class PushAtBottom {
 
-    public static void pushBottom(Stack<Integer> s,int data){
-        if(s.isEmpty()){
+    public static void pushBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
             s.push(data);
-            return ;
+            return;
         }
-        int top=s.pop();
-        pushBottom(s,data);
+        int top = s.pop();
+        pushBottom(s, data);
         s.push(top);
     }
+
     public static void main(String[] args) {
-     Stack<Integer> s=new Stack<>();
-     s.push(2);
-     s.push(8);
-     s.push(78);
-     pushBottom(s,4);
-     while(!s.isEmpty()){
-        System.out.println(s.pop() );
-     }
-        
+        Stack<Integer> s = new Stack<>();
+        s.push(2);
+        s.push(8);
+        s.push(78);
+        pushBottom(s, 4);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+
     }
 }
