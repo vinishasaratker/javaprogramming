82037414f1281e32518d859cbd77995cd7239791
vinishasaratker <vinishasaratker@gmail.com>
2026-07-12 22:28:00 +0530

    schh

--- function/basicfunc.java
+++ function/basicfunc.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
-public class basicfunc {
-    static void add() {
-     // here we didnt return any thing so that  we have use void
-        System.out.println(56 + 98);
-       }
-        String names(){
-        return "returnn "; 
-    }
-        
-       boolean add2(){
-        return true;
-        }
-        public static void main(String[] args) {
-        basicfunc obj=new basicfunc();
-  
-        obj.add();
-        System.out.println(obj.names());
-           System.out.println(obj.add2());
-      }
 
-        }
+public class basicfunc {
+   static void add() {
+      // here we didnt return any thing so that we have use void
+      System.out.println(56 + 98);
+   }
+
+   String names() {
+      return "returnn ";
+   }
+
+   boolean add2() {
+      return true;
+   }
+
+   public static void main(String[] args) {
+      basicfunc obj = new basicfunc();
+
+      obj.add();
+      System.out.println(obj.names());
+      System.out.println(obj.add2());
+   }
+
+}
--- strings/conversionuppercasetolowercase.java
+++ strings/conversionuppercasetolowercase.java
@@ -3,6 +3,7 @@ public class conversionuppercasetolowercase {
     public static void main(String[] args) {
         String str1 = "AbcdE";
         char ch[] = str1.toCharArray();
+        
         for (int i = 0; i < str1.length(); i++) {
             if (ch[i] >= 'A' && ch[i] <= 'Z') {
                 ch[i] = (char) (ch[i] + 32);
