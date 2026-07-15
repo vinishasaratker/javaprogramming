6360a0e1d8944178f9a56d776b8ac1d024430656
vinishasaratker <vinishasaratker@gmail.com>
2026-07-15 21:27:32 +0530

    data base

--- DivideAndConquere/Keysearch.java
+++ DivideAndConquere/Keysearch.java
@@ -1,48 +1,52 @@
 package DivideAndConquere;
+
 // this   problem is for rotated shorted array array rotate or short hona chahiye 
 public class Keysearch {
-public static int search(int arr[],int si,int tar,int ei){
-        if(si>ei)// base case 
-        { return -1 ;
+    public static int search(int arr[], int si, int tar, int ei) {
+        if (si > ei)// base case
+        {
+            return -1;
 
         }
 
-// kaam 
- int mid=si+(ei-si)/2;
- // case found 
- if(arr[mid]==tar){
-    return mid ;
- }
+        // kaam
+        int mid = si + (ei - si) / 2;
+        // case found
+        if (arr[mid] == tar) {
+            return mid;
+        }
 
- // mid lie of line l1
- if(arr[si]<=arr[mid]){
+        // mid lie of line l1
+        if (arr[si] <= arr[mid]) {
 
-// case 1 : jab  line l1 ke left pr hai 
-    if(arr[si]<=tar && tar<=arr[mid]){
-        return search(arr, si, tar, mid-1);
+            // case 1 : jab line l1 ke left pr hai
+            if (arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, si, tar, mid - 1);
+            }
+            // case b jab line l1 ke right pr lie kre
+            else {
+                return search(arr, mid + 1, tar, ei);
+            }
+        }
+
+        else {
+            // case c line l2 ka right
+            if (arr[mid] <= tar && tar <= arr[ei]) {
+                return search(arr, mid + 1, tar, ei);
+            }
+            // case d jab line l2 ke left side pr ho
+            else {
+
+                return search(arr, si, tar, mid - 1);
+            }
+
+        }
     }
-    // case b jab line  l1 ke right pr lie kre 
-    else{
-        return search(arr, mid+1, tar, ei);
-    }
- }
 
- else{
-    // case c  line l2 ka right 
-    if( arr[mid]<=tar && tar<=arr[ei]){
-        return search(arr, mid+1, tar, ei);
-    }
-// case d jab line l2 ke left side pr ho 
-else{
-
-    return search(arr, si, tar, mid-1);
-}
-
-}   }
     public static void main(String[] args) {
-        
-        int arr[]={4,5,6,7,0,1,2};
-       int targetIndex=search(arr, 0, 4, arr.length-1);
-   System.out.println(targetIndex);
-}
+
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int targetIndex = search(arr, 0, 4, arr.length - 1);
+        System.out.println(targetIndex);
+    }
 }
--- DivideAndConquere/QuickShort.java
+++ DivideAndConquere/QuickShort.java
@@ -1,56 +1,58 @@
 package DivideAndConquere;
 
 public class QuickShort {
-    
-    public static void printArr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-public static void quickshort(int arr[],int si,int ei){
-if(si>=ei){
-    return ;
-}
-
-    // last element 
-//partition pivod ka right index la kr deta hai 
- int pidx=partition(arr,si,ei);
- // left part
- quickshort(arr, si, pidx-1);
- // right part 
- quickshort(arr, pidx+1, ei);
-
-}
-
- public static int partition(int arr[],int si, int ei){
-
-    int pivot=arr[ei];
-    // i jagah bnayenga pivot se smaller  elemnts ke liye
-    int i=si-1;
-    for(int j=si;j<ei;j++){
-        if(arr[j]<=pivot){
-            i++;
-            // swap
-            int temp=arr[j];
-            arr[j]=arr[i];
-            arr[i]=temp ;
+    public static void quickshort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
         }
+
+        // last element
+
+        // partition pivod ka right index la kr deta hai
+
+        int pidx = partition(arr, si, ei);
+        // left part
+        quickshort(arr, si, pidx - 1);
+        // right part
+        quickshort(arr, pidx + 1, ei);
+
     }
 
+    public static int partition(int arr[], int si, int ei) {
 
-   i++;
-            // swap
-            int temp=pivot;
-            arr[ei]=arr[i];
-            arr[i]=temp ; 
-            return i;
-}
+        int pivot = arr[ei];
+        
+        // i jagah bnayenga pivot se smaller elemnts ke liye
+        int i = si - 1;
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        i++;
+        // swap
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
 
     public static void main(String[] args) {
-      int arr[]={6,3,8,2,5}; 
-      quickshort(arr, 0, arr.length-1); 
-      printArr(arr);
+        int arr[] = { 6, 3, 8, 2, 5 };
+        quickshort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
