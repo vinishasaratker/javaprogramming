be3cd46a5eb9993116a586302619ceeca00f9536
vinishasaratker <vinishasaratker@gmail.com>
2026-07-28 20:52:07 +0530

    code

--- arrayy/RemoveElement.java
+++ arrayy/RemoveElement.java
@@ -13,6 +13,25 @@ public class RemoveElement {
         return count;
     }
 
+
+static int sortedArrayRemoveElement(int[] arr) {
+
+    if (arr.length == 0)
+        return 0;
+
+    int count = 1;
+
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] != arr[i - 1]) {
+            arr[count] = arr[i];
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
     public static void main(String[] args) {
 
         int[] nums = {3, 2, 2, 3};
@@ -26,5 +45,17 @@ public class RemoveElement {
         for (int i = 0; i < k; i++) {
             System.out.print(nums[i] + " ");
         }
-    }
+
+
+
+int arr[] = {1, 2, 4, 4, 5, 33};
+
+int count = sortedArrayRemoveElement(arr);
+
+System.out.print("Array after removing duplicates: ");
+for (int i = 0; i < count; i++) {
+    System.out.print(arr[i] + " ");
+
+}
+}
 }
