70b100fc3127beb4a28c28ee6095fe005a90e423
vinishasaratker <vinishasaratker@gmail.com>
2026-08-03 08:39:54 +0530

    moves zeros to end

--- loops/Practice.java
+++ loops/Practice.java
@@ -1,9 +1,39 @@
 
  import java.util.Scanner;
-public class Practice
+public class Practice{
 
-{
+public static void  moveZeros(int []nums){
+int j=0 ;
+for(int i=0;i<nums.length;i++){
+    if(nums[i]!=0){
+        nums[j]=nums[i];
+    }
+     j++;
+    
+        nums[j]=nums[i];
+    
+   
+    for( j=0 ;j<nums.length;j++){
+ System.out.println(nums[j]);
+    }
+   
+}
+
+}
+
+
     public static void main(String[] args) {
+
+
+int nums[]={3,4,0,5,0,9,0};
+
+moveZeros(nums);
+
+
+
+
+
+
 //       Scanner sc= new Scanner(System.in);
 //       System.out.println("enter two number ");
 //       int n1=sc.nextInt();
@@ -72,25 +102,59 @@ int temp ;
 // number=number/10;
 
 
-System.out.println(" armstrong number ");
-int m=183 ;
-int tempr=m ;
-int store=0;
+// System.out.println(" armstrong number ");
+// int m=183 ;
+// int tempr=m ;
+// int store=0;
 
-while(m>0){
-    int lastno=m%10 ;
-    int ans=lastno*lastno*lastno;
- store=store +ans ;
-    m=m/10 ;
+// while(m>0){
+//     int lastno=m%10 ;
+//     int ans=lastno*lastno*lastno;
+//  store=store +ans ;
+//     m=m/10 ;
    
-}
+// }
 
-if(tempr==store){
-    System.out.println("armstrong ");
-}
-else{
-    System.out.println(" not armstrong ");
-}
+// if(tempr==store){
+//     System.out.println("armstrong ");
+// }
+// else{
+//     System.out.println(" not armstrong ");
+// }
+
+
+
+// System.out.println("febonachhi");
+
+// int  fi=0;
+// int si=1 ;
+// int third;
+// int n=10 ;
+// System.out.print(fi+" "+si);
+// for(int i=0;i<n ;i++){
+// third=fi+si ;
+// fi=si ;
+// si=third ;
+
+// System.out.print(" "+third);
+// }
+
+// System.out.println("check  prime number or not ");
+// int p=67 ;
+// int c=0;
+// for(int i=0 ;i<p;i++){
+//     if(p%i==0){
+// c++;
+//     }
+   
+// }
+//  if(c>2){
+//         System.out.println("not a prime number ");
+//     }
+//     else{
+//         System.out.println(" prime number ");
+//     }
+
 
 }
     }
