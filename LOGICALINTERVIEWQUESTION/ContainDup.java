50a19f12550adae3f51d34950d103327f5dac3b1
vinishasaratker <vinishasaratker@gmail.com>
2026-08-17 21:03:56 +0530

    priority queues

--- LOGICALINTERVIEWQUESTION/ContainDup.java
+++ LOGICALINTERVIEWQUESTION/ContainDup.java
@@ -1,30 +1,54 @@
 import java.util.*;
 
 class ContainDup {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
-        HashSet<Integer> hs = new HashSet<>();
+    //     HashSet<Integer> hs = new HashSet<>();
 
-        int arr[] = {3, 4, 2, 5, 6, 7, 5, 3, 2};
+    //     int arr[] = {3, 4, 2, 5, 6, 7, 5, 3, 2};
 
-        boolean duplicate = false;
+    //     boolean duplicate = false;
 
-        for (int x : arr) {
+    //     for (int x : arr) {
 
-            if (hs.contains(x)) {
-                duplicate = true;
-                break;
-            } 
-            else {
-                hs.add(x);
+    //         if (hs.contains(x)) {
+    //             duplicate = true;
+    //             break;
+    //         } 
+    //         else {
+    //             hs.add(x);
+    //         }
+    //     }
+
+    //     if (duplicate) {
+    //         System.out.println("Duplicate exists");
+    //     } 
+    //     else {
+    //         System.out.println("Duplicate does not exist");
+    //     }
+    // }
+
+
+    public static void intersection(int[] num1, int[] num2) {
+       HashSet<Integer> map=new HashSet<>(); 
+        for(int i=0;i<num1.length;i++){
+map.add(num1[i]);
+        }
+      
+        for(int j=0;j<num2.length;j++){
+            if(map.contains(num2[j]))
+            {
+               System.out.println(num2[j]);
+                map.remove(num2[j]);
             }
         }
+       
+    }
 
-        if (duplicate) {
-            System.out.println("Duplicate exists");
-        } 
-        else {
-            System.out.println("Duplicate does not exist");
-        }
+
+    public static void main(String[]args){
+        int num1[]={7,3,9};
+        int num2[]={6,3,9,2,9,4};
+intersection( num1, num2);
     }
 }
--- LOGICALINTERVIEWQUESTION/CountUniqueDigit.java
+++ LOGICALINTERVIEWQUESTION/CountUniqueDigit.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
  class CountUniqueDigit{
 
 static void countunique(int arr[]){
@@ -10,6 +11,11 @@ static void countunique(int arr[]){
       System.out.println(hs);
       System.out.println(" unique digit are:"+ hs.size());
 }
+
+
+
+
+
 static void firstnonRepeating( int arr2[]){
  HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -30,12 +36,48 @@ static void firstnonRepeating( int arr2[]){
 
 
 
+static void  unionIntersection(int num1[],int num2[]){
+HashSet<Integer>map=new HashSet<>();
+
+for(int i=0;i<num1.length;i++){
+    map.add(num1[i]);
+}
+for(int j=0;j<num2.length;j++){
+    map.add(num2[j]);
+}
+System.out.println("Union is :"+map.size());
+for(int key:map){
+    System.out.println(" " +key);
+}
+map.clear();
+
+for(int i=0 ;i<num1.length ;i++){
+    map.add(num1[i]);
+}
+int count=0;
+for(int j=0 ;j<num2.length ;j++){
+
+    if(map.contains(num2[j])){
+count ++ ;
+map.remove(num2[j]);
+    }
+    
+}
+
+System.out.println("  intersection is : " + count);
+
+}
+
 
 public static void main(String[]args){
  int[] arr = {1, 2, 2, 3, 3, 4};
  countunique(arr);
  int arr2[]={1,1,2,3,5,4,5,6,7,8};
  firstnonRepeating(arr2);
+
+ int num1[]={7,3,9};
+ int num2[]={6,3,9,2,9,4};
+ unionIntersection(num1,num2);
 }
 
 }
