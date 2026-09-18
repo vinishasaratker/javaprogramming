cfe8231e7c932fb3d91f6643f6647ee084c6cda2
vinishasaratker <vinishasaratker@gmail.com>
2026-09-18 13:55:12 +0530

    kth time rotate

---
+++ arrayy/KthTimeRotate.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class KthTimeRotate {
+
+    static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void RotateKthPlace(int arr[], int k, int n) {
+        k = k % n;
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter array size:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter value of k:");
+        int k = sc.nextInt();
+
+        RotateKthPlace(arr, k, n);
+
+        System.out.println("Array after rotating by " + k + " places:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
