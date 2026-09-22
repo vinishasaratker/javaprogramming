0358efc02c5d33ffacd8e01e2d93929e147020d4
vinishasaratker <vinishasaratker@gmail.com>
2026-09-22 17:45:35 +0530

    pairsumm

--- arrayy/PartitionInEqualSubArray.java
+++ arrayy/PartitionInEqualSubArray.java
@@ -2,12 +2,14 @@ class PartitionInEqualSubArray{
 static int FindSum(int arr[]){
 
 int totalsum=0;
+
 for(int i=0 ;i<arr.length;i++){
-    
+
     totalsum=totalsum+arr[i];
 
 }
 return totalsum ;
+
 }
 static boolean partition(int arr[]){
 
@@ -21,6 +23,7 @@ for(int i=0;i<arr.length;i++){
     int suffixsum=sum-prefixsum;
     if(prefixsum==suffixsum){
         return false ;
+
     }
 
 }
@@ -30,7 +33,8 @@ return true ;
 }
 public static void main(String[] args){
 
-    int arr[]={10,5,6,4,2,3};
+    int arr[]=  {10,5,6,4,2,3}  ;
+
     System.out.println(" sum is :"+partition(arr));
 }
 
--- arrayy/pairsum.java
+++ arrayy/pairsum.java
@@ -1,26 +1,46 @@
-public class pairsum{
-   static void ps(int arr5[]){
+public class pairsum
+
+{
+   static void ps(int arr5[])
+   {
      int target=5;
+
      int n=arr5.length;
+
       int keyarray[]=new int[100];
+
       int index=0;
+
       for(int i=0;i<arr5.length;i++){
 
-         for(int j=i+1;j<arr5.length;j++){
+     for(int j=i+1;j<arr5.length;j++)
+         {
    if(arr5[i]+arr5[j]==target){
+
    int num1=arr5[i];
+
    int num2=arr5[j];
+
    if(num1>num2){
+
       int temp=num1;
+
       num1=num2;
+
       num2=temp;
-   }
+     }
+       
         int keypair=num1*10+num2;
-               boolean dup=false;
-   for(int k=0;k<index;k++){
+
+    boolean dup=false;
+
+    for(int k=0;k<index;k++){
       
-      if(keypair==keyarray[k]){
+      if(keypair==keyarray[k])
+      {
+
          dup=true;
+
          break;
          
       }
@@ -37,7 +57,9 @@ public class pairsum{
       
        }
    public static void main(String[] args) {
-      int arr5[]={1,2,3,4,5,4,3,2,1,4,5,6,0};
+
+      int arr5[]={1,2,3,4,5,4,3,2,15,6,0};
+
       ps(arr5);
    }
 }
