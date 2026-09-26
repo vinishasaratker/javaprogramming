f83b29b20173d52dcd0e0ca6010db6c98d6ce2fc
vinishasaratker <vinishasaratker@gmail.com>
2026-09-26 23:06:16 +0530

    loops

--- Backtracking/Hello.java
+++ Backtracking/Hello.java
@@ -2,5 +2,8 @@ class Hello{
 
     public static void main (String[]args){
         System.out.println("hello");
+        for(int i=0;i<6;i++){
+            System.out.println("pagal");
+        }
     }
 }
